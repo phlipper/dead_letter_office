192f1c851cae9874183738c582331da4e0e1e189
Phil Cohen <github@phlippers.net>
2012-08-05 22:47:39 -0700

    setup travis database configs based on RUBY_ENGINE

--- spec/ci/before_script.sh
+++ spec/ci/before_script.sh
@@ -1,15 +1,15 @@
 rm -f spec/dummy/config/database.yml
 
+engine=$(ruby -e 'puts RUBY_ENGINE')
+
 case $DB in
   "mysql" )
-    mysql -e 'CREATE DATABASE anedot_test;'
-    cp spec/ci/database.mysql.yml spec/dummy/config/database.yml;;
+    mysql -e 'CREATE DATABASE anedot_test;';;
   "postgres" )
-    psql -c 'CREATE DATABASE anedot_test;' -U postgres
-    cp spec/ci/database.postgresql.yml spec/dummy/config/database.yml;;
-  "sqlite" )
-    cp spec/ci/database.sqlite.yml spec/dummy/config/database.yml;;
+    psql -c 'CREATE DATABASE anedot_test;' -U postgres;;
 esac
+
+cp spec/ci/database.$DB.$engine.yml spec/dummy/config/database.yml
 
 cd spec/dummy
 RAILS_ENV=test bundle exec rake db:setup db:migrate
