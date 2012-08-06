6f31be2215d9a547ef73745185d84e2b94fa02fb
Phil Cohen <github@phlippers.net>
2012-08-05 21:34:42 -0700

    make `date` column datetime datatype

--- db/migrate/20120712054220_create_dead_letter_office_dead_letters.rb
+++ db/migrate/20120712054220_create_dead_letter_office_dead_letters.rb
@@ -1,17 +1,17 @@
 class CreateDeadLetterOfficeDeadLetters < ActiveRecord::Migration
   def change
     create_table :dead_letter_office_dead_letters do |t|
-      t.string :mailer
-      t.string :message_id
-      t.string :subject
-      t.string :to
-      t.string :from
-      t.string :cc
-      t.string :bcc
-      t.time   :date
-      t.text   :mail
-      t.string :exception_name
-      t.string :exception_message
+      t.string   :mailer
+      t.string   :message_id
+      t.string   :subject
+      t.string   :to
+      t.string   :from
+      t.string   :cc
+      t.string   :bcc
+      t.datetime :date
+      t.text     :mail
+      t.string   :exception_name
+      t.string   :exception_message
 
       t.timestamps
     end
