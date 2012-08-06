09ed8d4f7ae1e1c38163ed17cf7a310b6723ab94
Phil Cohen <github@phlippers.net>
2012-08-05 22:08:34 -0700

    initial setup for travis ci

---
+++ spec/ci/before_script.sh
@@ -0,0 +1,14 @@
+rm -f spec/dummy/config/database.yml
+
+case $DB in
+  "mysql" )
+    mysql -e 'CREATE DATABASE anedot_test;'
+    cp spec/ci/database.mysql.yml spec/dummy/config/database.yml;;
+  "postgres" )
+    psql -c 'CREATE DATABASE anedot_test;' -U postgres
+    cp spec/ci/database.postgresql.yml spec/dummy/config/database.yml;;
+  "sqlite" )
+    cp spec/ci/database.sqlite.yml spec/dummy/config/database.yml;;
+esac
+
+RAILS_ENV=test bundle exec rake db:setup db:migrate
